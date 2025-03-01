f23266fedcdce50225d2d77865d17300ccaaf2a6
Sreyas <72121982+syedrey@users.noreply.github.com>
2025-03-01 20:25:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,15 +17,25 @@ def handle_joystick_data(data):
     motor1_speed = int(data['joystick1']['y'])  # Range: -100 to 100
     motor1_speed = max(-100, min(100, motor1_speed))  # Clamping to valid range
 
-    # **Motor 2 and Motor 3 logic remains the same**
-    if data['joystick2']['x'] < 0:
-        motor2_speed = int((data['joystick2']['x'] + 100) * 1.275)
-        motor2_speed = max(0, min(255, motor2_speed))
-        motor3_speed = 0
+    motor2_speed = 0
+    motor3_speed = 0
+
+    # **Fixing Motor 2 & 3 behavior**
+    if data['joystick2']['y'] > 10:  # Forward
+        motor2_speed = data['joystick2']['y']
+        motor3_speed = data['joystick2']['y']
+    elif data['joystick2']['y'] < -10:  # Reverse
+        motor2_speed = data['joystick2']['y']
+        motor3_speed = data['joystick2']['y']
+    elif data['joystick2']['x'] < -10:  # Left Turn
+        motor2_speed = 100   # Full Forward
+        motor3_speed = -100  # Full Reverse
+    elif data['joystick2']['x'] > 10:  # Right Turn
+        motor2_speed = -100  # Full Reverse
+        motor3_speed = 100   # Full Forward
     else:
-        motor3_speed = int((data['joystick2']['x'] + 100) * 1.275)
-        motor3_speed = max(0, min(255, motor3_speed))
         motor2_speed = 0
+        motor3_speed = 0  # Reset motors to neutral when joystick is centered
 
     # Send corrected values to Arduino
     arduino.write(f"{motor1_speed},{motor2_speed},{motor3_speed}\n".encode())
